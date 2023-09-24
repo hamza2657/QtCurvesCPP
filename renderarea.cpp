a1674d350f0bb6bd60a027ef377be74357985bdc
Muhammad Hamza <hamxa2657@gmail.com>
2023-09-24 16:39:45 +0500

    Drawing shapes using line instead of dots

--- renderarea.cpp
+++ renderarea.cpp
@@ -129,13 +129,18 @@ void RenderArea::paintEvent(QPaintEvent *event)
     //drawig area
     painter.drawRect(this->rect());
     QPoint center = this->rect().center();
+    QPointF prevPoint = compute(0);
+    QPoint prevPixel;
+    prevPixel.setX(prevPoint.x() * mScale + center.x());
+    prevPixel.setY(prevPoint.y() * mScale + center.y());
     float step = mIntervalLength / mStepCount;
     for (float t = 0; t < mIntervalLength; t+=step) {
         QPointF point = compute (t);
         QPoint pixel;
         pixel.setX(point.x() * mScale + center.x());
         pixel.setY(point.y() * mScale + center.y());
-        painter.drawPoint(pixel);
+        painter.drawLine(pixel,prevPixel);
+        prevPixel = pixel;
 
     }
 }
