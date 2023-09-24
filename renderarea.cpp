91bf847b8a37d4e77e67da95bd86e08a774911b9
Muhammad Hamza <hamxa2657@gmail.com>
2023-09-24 16:03:22 +0500

    code handles curves dynamically

--- renderarea.cpp
+++ renderarea.cpp
@@ -8,11 +8,54 @@ RenderArea::RenderArea(QWidget *parent):
     QWidget{parent},mBackgroundColor (0,0,255), mShapeColor (255,255,255)
     ,mShape(Astroid)
 {
+    on_shape_changed();
+}
 
+QPointF RenderArea::compute(float t)
+{
+    switch (mShape) {
+    case Astroid:
+        return compute_astroid(t);
+        break;
+    case Cycloid:
+        return compute_cycloid(t);
+        break;
+    case HuygensCycloid:
+        return compute_huygens(t);
+        break;
+    case HypoCycloid:
+        return compute_hypo(t);
+    case lineCurve:
+        return compute_curve(t);
+        break;
+    default:
+        break;
+    }
+    return QPoint(0,0);
 }
 QSize RenderArea::minimumSizeHint() const
 {
     return QSize(100,100);
+}
+void RenderArea::on_shape_changed()
+{
+    switch (mShape) {
+    case Astroid:
+        mScale = 40;
+        mIntervalLength = 2 * M_PI;
+        mStepCount = 256;
+        break;
+    case Cycloid:
+        break;
+    case HuygensCycloid:
+        break;
+    case HypoCycloid:
+        break;
+    case lineCurve:
+        break;
+    default:
+        break;
+    }
 }
 QPointF RenderArea::compute_astroid(float t)
 {
@@ -21,6 +64,22 @@ QPointF RenderArea::compute_astroid(float t)
     float x  = 2 * cos_t * cos_t * cos_t; // pow(cos_t, 3)
     float y  = 2 * sin_t * sin_t * sin_t; // pow(sin_t, 3)
     return QPointF (x,y);
+}
+QPointF RenderArea::compute_curve(float t)
+{
+
+}
+QPointF RenderArea::compute_cycloid(float t)
+{
+
+}
+QPointF RenderArea::compute_huygens(float t)
+{
+
+}
+QPointF RenderArea::compute_hypo(float t)
+{
+
 }
 QSize RenderArea::sizeHint() const
 {
@@ -31,39 +90,18 @@ void RenderArea::paintEvent(QPaintEvent *event)
     Q_UNUSED (event);
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing,true);
-
-
-    switch (mShape) {
-    case Astroid:
-        mBackgroundColor = Qt::red;
-        break;
-    case Cycloid:
-        mBackgroundColor = Qt::green;
-        break;
-    case HuygensCycloid:
-        mBackgroundColor = Qt::blue;
-        break;
-    case HypoCycloid:
-        mBackgroundColor = Qt::yellow;
-        break;
-    default:
-        break;
-    }
     painter.setBrush(mBackgroundColor);
     painter.setPen(mShapeColor);
 
     //drawig area
     painter.drawRect(this->rect());
     QPoint center = this->rect().center();
-    int stepCount = 256;
-    float scale = 40;
-    float intervalLength = 2* M_PI;
-    float step = intervalLength / stepCount;
-    for (float t = 0; t < intervalLength; t+=step) {
-        QPointF point = compute_astroid(t);
+    float step = mIntervalLength / mStepCount;
+    for (float t = 0; t < mIntervalLength; t+=step) {
+        QPointF point = compute (t);
         QPoint pixel;
-        pixel.setX(point.x() * scale + center.x());
-        pixel.setY(point.y() * scale + center.y());
+        pixel.setX(point.x() * mScale + center.x());
+        pixel.setY(point.y() * mScale + center.y());
         painter.drawPoint(pixel);
 
     }
--- renderarea.h
+++ renderarea.h
@@ -15,12 +15,13 @@ public:
         Astroid,
         Cycloid,
         HuygensCycloid,
-        HypoCycloid
+        HypoCycloid,
+        lineCurve
 
     };
     void setBackgroundColor(QColor color){mBackgroundColor = color;} //setter
     QColor backgroundColor() const {return mBackgroundColor;} //getter
-    void setShape(ShapeType shape) {mShape = shape;}
+    void setShape(ShapeType shape) {mShape = shape; on_shape_changed();}
     ShapeType shape () const {return mShape;}
 protected:
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
@@ -28,11 +29,20 @@ protected:
 
 signals:
 private:
+    void on_shape_changed();
+    QPointF compute(float t);//dispatch function based on mShape Types
     QPointF compute_astroid(float t);
+    QPointF compute_cycloid(float t);
+    QPointF compute_huygens(float t);
+    QPointF compute_hypo(float t);
+    QPointF compute_curve(float t);
 private:
     QColor mBackgroundColor;
     QColor mShapeColor;
     ShapeType mShape;
+    float mIntervalLength;
+    float mScale;
+    int mStepCount;
 
 
 };
